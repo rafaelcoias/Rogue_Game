d482b5d9bfc5ed6af39d203a58574778da3baabe
Rafael Coias <rafael.p.coias@hotmail.com>
2022-10-27 13:26:40 +0100

    Update

--- Rogue_Rafael_Coias_105361/src/pt/iscte/poo/example/Main.java
+++ Rogue_Rafael_Coias_105361/src/pt/iscte/poo/example/Main.java
@@ -2,7 +2,7 @@ package pt.iscte.poo.example;
 
 public class Main {
 	
-	// O programa começa aqui e inicia o jogo pela funcao start();
+	// The program starts here.
 	
 	public static void main(String[] args) {
 		Engine.getInstance().start();
